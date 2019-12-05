fa383f9fac7596081362d00d93379db058992138
Vahe Yeritsyan <vahe.yeritsyan.2001@gmail.com>
2019-12-05 16:03:35 +0400

    Motivational commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-	System.out.println("Some change");
+        System.out.println("Don't even dream about it, you are not going to be accepted");
+
     }
 }
 //well here is my contribution
